9112c4929c17ccdcd88c03d7e208446cbb25c1b7
TatakAdi <tatakadisubagyo@gmail.com>
2025-08-30 15:09:07 +0800

    feat: Dropdown menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
+// Variabel Slider
 let slides = document.querySelectorAll(".slide");
 const slider = document.querySelector(".slider");
 const totalSlides = slides.length;
 let currentIndex = 0;
 
+//Variabel Dropdown Menu
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.getElementById("menu");
+
+// Slider Logic DOM
 function showSlide(index) {
   slider.style.transform = `translateX(-${index * 100}%)`;
 }
@@ -22,3 +28,8 @@ document.getElementById("prev").addEventListener("click", prevSlide);
 document.getElementById("next").addEventListener("click", nextSlide);
 
 setInterval(nextSlide, 5000);
+
+// Dropdown Menu DOM Logic
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
